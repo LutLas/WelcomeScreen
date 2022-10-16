12af794abe1ab1fe3122d89ee0cf612312413946
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-10-16 11:59:33 +0200

    Initial Commit

---
+++ WelcomeScreen/WelcomeScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeScreenApp.swift
+//  WelcomeScreen
+//
+//  Created by Lutaka Muyoba Chihota on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct WelcomeScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
