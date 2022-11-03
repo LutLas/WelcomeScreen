4024733ca4727ace17ee536a0226cc88f8dbf9f9
LutLastic Fantastic <50748557+LutLas@users.noreply.github.com>
2022-11-03 06:26:27 +0200

    Welcome Screen
    Example of a simple welcome screen in swift

--- WelcomeScreen/ContentView.swift
+++ WelcomeScreen/ContentView.swift
@@ -9,13 +9,62 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text("Welcome to ")
+                .font(.system(size: 50))
+                .fontWeight(.heavy)
+                .foregroundColor(.primary)
+            Text("Swift Pizza")
+                .font(.system(size: 50))
+                .fontWeight(.heavy)
+                .foregroundColor(Color(UIColor.systemBlue))
+                .opacity(0.8)
+            Spacer()
+            VStack(alignment: .leading, spacing: 24){
+                HStack(alignment: .top, spacing: 20){
+                    Image(systemName: "bag").resizable()
+                        .frame(width: 40,height: 40)
+                        .foregroundColor(Color(UIColor.systemBlue))
+                        .opacity(0.8)
+                    VStack(alignment: .leading, spacing: 4){
+                        Text("Order Everything").font(.headline).bold()
+                        Text("Our whole menu is the app.").font(.subheadline)
+                    }
+                }
+                HStack(alignment: .top, spacing: 20){
+                    Image(systemName: "giftcard").resizable()
+                        .frame(width: 40,height: 40)
+                        .foregroundColor(Color(UIColor.systemBlue))
+                        .opacity(0.8)
+                    VStack(alignment: .leading, spacing: 4){
+                        Text("Buy Gift Card").font(.headline).bold()
+                        Text("Buy gift card, check card balance, and more.").font(.subheadline)
+                    }
+                }
+                HStack(alignment: .top, spacing: 20){
+                    Image(systemName: "fork.knife").resizable()
+                        .frame(width: 40,height: 40)
+                        .foregroundColor(Color(UIColor.systemBlue))
+                        .opacity(0.8)
+                    VStack(alignment: .leading, spacing: 4){
+                        Text("Customize Pizzas").font(.headline).bold()
+                        Text("Pick and choose ingredients to make your pizza.").font(.subheadline)
+                    }
+                }
+            }
+            Spacer()
+            Button(action: {
+                
+            }, label: {
+                Text("Welcome")
+                    .foregroundColor(.white)
+                    .bold()
+            }).frame(width: 350,height: 60)
+                .background(Color(UIColor.systemBlue)
+                    .opacity(0.8))
+                .cornerRadius(12)
         }
-        .padding()
+        .padding(.all, 40)
     }
 }
 
